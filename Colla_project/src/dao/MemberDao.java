f0e84a9ca75c40481f1b896370f8aaee70206fae
bitcamp504 <bitcamp504@DESKTOP-BOP5QHQ>
2019-08-20 17:49:33 +0900

    멤버~~

--- Colla_project/src/dao/MemberDao.java
+++ Colla_project/src/dao/MemberDao.java
@@ -1,5 +1,13 @@
 package dao;
 
-public interface MemberDao {
+import java.util.List;
 
+import model.Member;
+
+public interface MemberDao {
+	public int insertMember(Member member);
+	public int updateMember(Member member);
+	public int deleteMember(int mNum);
+	public Member selectMemberByNum(int mNum);
+	public List<Member> selectAllMember();
 }
--- Colla_project/src/dao/mapper/memberMapper.java
+++
@@ -1,5 +0,0 @@
-package dao.mapper;
-
-public class memberMapper {
-
-}
