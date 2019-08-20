d8c261a248638dde5ec1884f25faca9803d6ea1f
bitcamp <bitcamp@DESKTOP-LR3GM3V>
2019-08-20 17:39:13 +0900

    보드서비스 만들었습니다

--- Colla_project/src/model/Board.java
+++ Colla_project/src/model/Board.java
@@ -1,5 +1,54 @@
 package model;
 
-public class Board {
+import java.util.Date;
 
+public class Board {
+	private int num;
+	private String title;
+	private String content;
+	private int mNum;
+	private Date regDate;
+	private int readCount;
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getmNum() {
+		return mNum;
+	}
+	public void setmNum(int mNum) {
+		this.mNum = mNum;
+	}
+	public Date getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+	public int getReadCount() {
+		return readCount;
+	}
+	public void setReadCount(int readCount) {
+		this.readCount = readCount;
+	}
+	@Override
+	public String toString() {
+		return "Board [num=" + num + ", title=" + title + ", content=" + content + ", mNum=" + mNum + ", regDate="
+				+ regDate + ", readCount=" + readCount + "]";
+	}
+	
 }
