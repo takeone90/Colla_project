344d5311b6af54a2659105dfb0f900afe67a3d35
bitcamp <bitcamp@DESKTOP-KG6G7I3>
2019-08-20 18:35:28 +0900

    controller

--- Colla_project/src/controller/BoardController.java
+++ Colla_project/src/controller/BoardController.java
@@ -9,8 +9,19 @@ import service.BoardService;
 @Controller
 public class BoardController {
 	private BoardService service;
+	
 	@RequestMapping("/boardList")
-	public String main(Model model) {
+	public String showBoardList(Model model) {
 		return "boardList";
+	}
+	
+	@RequestMapping("/boardWriteForm")
+	public String showBoardWriteForm() {
+		return "boardWriteForm";
+	}
+	
+	@RequestMapping("/boardModifyForm")
+	public String showBoardModifyForm() {
+		return "boardModifyForm";
 	}
 }
