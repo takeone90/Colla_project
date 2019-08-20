d12b30f2b13d8f348bb817d11c51d72ee9b5772d
bitcamp <bitcamp@DESKTOP-LR3GM3V>
2019-08-20 18:30:04 +0900

    gd

--- Colla_project/src/controller/BoardController.java
+++ Colla_project/src/controller/BoardController.java
@@ -1,5 +1,16 @@
 package controller;
 
-public class BoardController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import service.BoardService;
+
+@Controller
+public class BoardController {
+	private BoardService service;
+	@RequestMapping("/main")
+	public String main(Model model) {
+		return "main";
+	}
 }
--- Colla_project/src/dao/BoardDao.java
+++ Colla_project/src/dao/BoardDao.java
@@ -1,5 +1,13 @@
 package dao;
 
-public class BoardDao {
+import java.util.List;
 
+import model.Board;
+
+public interface BoardDao {
+	public int insertBoard(Board board);
+	public int updateBoard(Board board);
+	public int deleteBoard(int num);
+	public Board selectOneBoard(int num);
+	public List<Board> selectAllBoard();
 }
