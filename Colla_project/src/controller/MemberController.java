293bc21737a2992375f35ab9bf23ff035ffa1b4c
bitcamp504 <bitcamp504@DESKTOP-BOP5QHQ>
2019-08-20 18:20:20 +0900

    풀~~

--- Colla_project/src/controller/MemberController.java
+++ Colla_project/src/controller/MemberController.java
@@ -1,5 +1,36 @@
 package controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import service.MemberService;
+
+@Controller
+@RequestMapping("/member")
 public class MemberController {
- 
+
+	@Autowired
+	MemberService memberService;
+	
+	//회원가입 페이지 
+	@RequestMapping(value = "join", method = RequestMethod.GET)
+	public String joinForm() {
+		return "joinForm";
+	}
+	//회원가입 로직
+	@RequestMapping(value = "join", method = RequestMethod.POST)
+	public String join() {
+		 
+		return "result";
+	}
+	//회원정보수정 페이지
+	//회원정보수정 로직
+	//로그인 페이지
+	//로그인 로직
+	//로그아웃 로직
+	//회원 탈퇴 로직
+	
 }
--- Colla_project/src/service/MemberService.java
+++ Colla_project/src/service/MemberService.java
@@ -1,27 +1,55 @@
 package service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import dao.BoardDao;
+import dao.MemberDao;
 import model.Member;
 
 @Service
 public class MemberService {
-
+	//addMember
+	//modifyMember
+	//deleteMember
+	//addMember
+	//selectMemberByNum
+	//selectMemberList
+	
 	@Autowired
-	BoardDao boardDao;
+	MemberDao memberDao;
 
 	public boolean addMember(Member member) {
-
+		if(memberDao.insertMember(member)>0) {
+			return true;
+		}else {
+			return false;
+		}
 	}
 
 	public boolean modifyMember(Member member) {
-
+		if(memberDao.updateMember(member)>0) {
+			return true;
+		}else {
+			return false;
+		}
 	}
 	
-	public boolean deleteMember(Member member) {
-
+	public boolean deleteMember(int mNum) {
+		if(memberDao.deleteMember(mNum)>0) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	public Member selectMemberByNum(int mNum) {
+		return memberDao.selectMemberByNum(mNum);
+	} 
+	
+	public List<Member> selectMemberList(){
+		return memberDao.selectAllMember();
 	}
 
 }
