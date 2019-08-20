320cca02ff7e451682939454aed62de7dba114cf
bitcamp <bitcamp@DESKTOP-KG6G7I3>
2019-08-20 18:49:22 +0900

    boardController 수정

--- Colla_project/src/controller/BoardController.java
+++ Colla_project/src/controller/BoardController.java
@@ -6,22 +6,29 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import model.Board;
+import model.Member;
 import service.BoardService;
+import service.MemberService;
 
 @Controller
 public class BoardController {
 	@Autowired
-	private BoardService service;
+	private BoardService bService;
+	
+	@Autowired
+	private MemberService mService;
 	
 	@RequestMapping("/boardList")
 	public String showBoardList(Model model) {
-		model.addAttribute("bList", service.selectAll());
+		model.addAttribute("bList", bService.selectAll());
 		return "boardList";
 	}
 	@RequestMapping("/boardView")
 	public String showBoardView(int num,Model model) {
-		Board board = service.selectBoard(num);
+		Board board = bService.selectBoard(num);
 		model.addAttribute("board", board);
+		Member m = mService.selectMemberByNum( board.getmNum() );
+		model.addAttribute("writer", m.getmName());
 		return "boardView";
 	}
 	@RequestMapping("/boardWriteForm")
