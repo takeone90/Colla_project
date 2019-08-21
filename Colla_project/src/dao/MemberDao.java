96ff8a53080ba14a719d78c4cda647016483e854
bitcamp <bitcamp@DESKTOP-LR3GM3V>
2019-08-21 15:08:25 +0900

    member, memberdao, memberservice //수빈 (시큐리티x)

--- Colla_project/src/dao/MemberDao.java
+++ Colla_project/src/dao/MemberDao.java
@@ -1,5 +1,15 @@
 package dao;
 
-public interface MemberDao {
+import java.util.List;
 
+import model.Member;
+
+public interface MemberDao {
+	public int insertMember(Member member);
+	public int updateMember(Member member);
+	public int deleteMember(int num);
+	public Member selectMember(int num);
+	public Member selectMemberByEmail(String email);
+	public List<Member> selectAll();
+	
 }
