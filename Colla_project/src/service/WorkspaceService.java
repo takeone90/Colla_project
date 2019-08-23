c58a4232c0a1245e2f433d2edcd230e0b0b66e25
bitcamp <bitcamp@DESKTOP-LR3GM3V>
2019-08-23 10:59:08 +0900

    workspace수정 / 수빈

--- Colla_project/src/service/WorkspaceService.java
+++ Colla_project/src/service/WorkspaceService.java
@@ -2,6 +2,7 @@ package service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import dao.WorkspaceDao;
@@ -9,6 +10,7 @@ import model.Workspace;
 
 @Service
 public class WorkspaceService {
+	@Autowired
 	private WorkspaceDao dao;
 	public boolean addWorkspace(int mNum,String name) {
 		boolean result = false;
--- Colla_project/src/test/test.java
+++ Colla_project/src/test/test.java
@@ -1,19 +1,24 @@
 package test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import service.MemberService;
+import service.WorkspaceService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:WebContent/WEB-INF/spring/root-context.xml")
-public class Test {
+public class test {
+	@Autowired
+	WorkspaceService wservice;
 	@Autowired
 	MemberService service;
-	@org.junit.Test
+	@Test
 	public void serviceTest() {
-		
+		System.out.println(service.getAllMember());
+		System.out.println(wservice.getAllWorkspace());
 	}
 }
