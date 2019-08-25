3504d9cbe5bcc8a3a21847f9fe9968cc06ecf78b
bitcamp <lightning350@naver.com>
2019-08-25 17:41:47 +0900

    캘린더 수정 중

--- Colla_project/src/controller/CalendarController.java
+++ Colla_project/src/controller/CalendarController.java
@@ -1,11 +1,20 @@
 package controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import model.Calendar;
+import service.CalendarService;
 
 @Controller
 public class CalendarController {
+	
+	@Autowired
+	private CalendarService calendarService;
 	
 	@RequestMapping(value="/calMonth", method = RequestMethod.GET)
 	public String showCalMonth() {
@@ -22,9 +31,21 @@ public class CalendarController {
 		return "/calendar/calSearchList";
 	}
 	
-	@RequestMapping(value="/addSchedule")
-	public String addSchedule() {
-		
-		return "";
+	@ResponseBody
+	@RequestMapping(value="/addSchedule", method = RequestMethod.GET)
+	public boolean addSchedule(Calendar data, int yearCalendar, int annually, int monthly) {
+		System.out.println("addSchedule");
+		System.out.println(data);
+		return calendarService.addCalendar(data);
+	}
+	@ResponseBody
+	@RequestMapping(value="/modifySchedule")
+	public boolean modifySchedule(Calendar calendar) {
+		return calendarService.modifyCalendar(calendar);
+	}
+	@ResponseBody
+	@RequestMapping(value="/removeSchedule")
+	public boolean removeSchedule(int cNum) {
+		return calendarService.removeCalendar(cNum);
 	}
 }
--- Colla_project/src/service/CalendarService.java
+++ Colla_project/src/service/CalendarService.java
@@ -1,5 +1,42 @@
 package service;
 
-public class CalendarService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import dao.CalendarDao;
+import model.Calendar;
+
+
+
+@Service
+public class CalendarService {
+	@Autowired
+	private CalendarDao calendarDao;
+	
+	public boolean addCalendar(Calendar calendar) {
+		if(calendarDao.insertCalendar(calendar)>0) {
+			return true;
+		}
+		return false;
+	}
+	public boolean modifyCalendar(Calendar calendar) {
+		if(calendarDao.updateCalendar(calendar)>0) {
+			return true;
+		}
+		return false;
+	}
+	public boolean removeCalendar(int cNum) {
+		if(calendarDao.deleteCalendar(cNum)>0) {
+			return true;
+		}
+		return false;
+	}
+	public Calendar getCalendar(int cNum) {
+		return calendarDao.selectCalendar(cNum);
+	}
+	public List<Calendar> getAllCalendar() {
+		return calendarDao.selectAllCalendar();
+	}
 }
