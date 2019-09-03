b3eb0a3845803481bb16f8c7a7ddcbfd3394912b
benefitallhumankind <benefitallhumankind@gmail.com>
2019-09-03 12:31:27 +0900

    download 수정

--- Colla_project/src/controller/MainController.java
+++ Colla_project/src/controller/MainController.java
@@ -1,10 +1,18 @@
 package controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.View;
+
+import service.FileService;
 
 @Controller
 public class MainController {
+	
+	@Autowired
+	private FileService fService;
+	
 	@RequestMapping("/main")
 	public String main() {
 		return "/main/main";
@@ -12,5 +20,9 @@ public class MainController {
 	@RequestMapping("/error")
 	public String error() {
 		return "/error/error";
+	}
+	@RequestMapping("/download")
+	public View download(String name) {
+		return fService.getDownload(name);
 	}
 }
