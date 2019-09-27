5e8a947770096ff79bc13c37b3dd769b8dff8612
bitcamp <lightning350@naver.com>
2019-09-27 15:19:29 +0900

    프로젝트2

--- Colla_project/src/controller/ProjectController.java
+++ Colla_project/src/controller/ProjectController.java
@@ -1,20 +1,26 @@
 package controller;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import model.ChatRoom;
 import model.Project;
+import model.ProjectMember;
 import model.Workspace;
 import service.ChatRoomService;
+import service.ProjectMemberService;
 import service.ProjectService;
 import service.WorkspaceService;
 
@@ -23,20 +29,35 @@ public class ProjectController {
 	@Autowired
 	private ProjectService pService;
 	@Autowired
+	private ProjectMemberService pmService;
+	@Autowired
 	private ChatRoomService crService;
 	@Autowired
 	private WorkspaceService wService;
 	
 	@RequestMapping("/projectMain") //projectMain으로 이동
-	public String showProjectMain(HttpSession session, int crNum, int wNum) {
-		
-		List<Project> pList = pService.getAllProjectByWnum(wNum);
+	public String showProjectMain(HttpSession session, int wNum, Model model) {
+		List<Project> pList = pService.getAllProjectByWnum(wNum); //프로젝트 리스트를 가져온다..
+		System.out.println("pList : "+pList);
+		List<Map<String, Object>> projectList = new ArrayList<Map<String,Object>>();
+		for(int i=0; i<pList.size(); i++) { //프로젝트 리스트를 돌면서..
+			int pNum = pList.get(i).getpNum();
+			System.out.println("pNum : "+pNum);
+			List<ProjectMember> pmList = pmService.getAllProjectMemberByPnum(pNum); // 각각의 프로젝트에 속한 멤버 가져오기
+			System.out.println("pmList : "+pmList);
+			Map<String, Object> pMap = new HashMap<String, Object>();
+			pMap.put("pmInfo", pList.get(i)); //프로젝트 정보 
+			pMap.put("pmList", pmList); //프로젝트 소속 멤버
+			System.out.println("pMap : "+pMap);
+			projectList.add(pMap);
+		}
+		model.addAttribute("projectList", projectList);
 		
 		Workspace ws = wService.getWorkspace(wNum);
-		ChatRoom chatRoom = crService.getChatRoomByCrNum(crNum);
+//		ChatRoom chatRoom = crService.getChatRoomByCrNum(crNum);
 		session.setAttribute("wsName", ws.getName());
 		session.setAttribute("currWnum", wNum);
-		session.setAttribute("sessionChatRoom", chatRoom);		
+//		session.setAttribute("sessionChatRoom", chatRoom);		
 		return "/project/projectMain";
 	}
 	
