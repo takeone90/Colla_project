67310f450553b9298ed560d4f84fa760766de654
bitcamp <bitcamp@DESKTOP-LR3GM3V>
2019-10-01 11:17:08 +0900

    project member 초대 가능

--- Colla_project/src/model/WsMember.java
+++ Colla_project/src/model/WsMember.java
@@ -4,6 +4,19 @@ public class WsMember {
 	private int wmNum;
 	private int wNum;
 	private int mNum;
+	private String mName;
+	public int getWmNum() {
+		return wmNum;
+	}
+	public void setWmNum(int wmNum) {
+		this.wmNum = wmNum;
+	}
+	public String getmName() {
+		return mName;
+	}
+	public void setmName(String mName) {
+		this.mName = mName;
+	}
 	public int getWsNum() {
 		return wmNum;
 	}
@@ -24,7 +37,7 @@ public class WsMember {
 	}
 	@Override
 	public String toString() {
-		return "워크스페이스참여자정보 [wsMember정보테이블 번호 : " + wmNum + ", workspace번호 : " + wNum + ", 참여멤버번호 : " + mNum + "]";
+		return "워크스페이스참여자정보 [wsMember정보테이블 번호 : " + wmNum + ", workspace번호 : " + wNum + ", 참여멤버번호 : " + mNum + ", 참여멤버이름 : "+mName+"]";
 	}
 	
 }
