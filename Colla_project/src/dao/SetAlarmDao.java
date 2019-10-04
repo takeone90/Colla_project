feaf7443e43abfd67228a3940594cf9f2a621ad9
bitcamp504 <bitcamp504@DESKTOP-BOP5QHQ>
2019-10-04 15:35:33 +0900

    미경 : setAlarm mapper 수정!

--- Colla_project/src/dao/SetAlarmDao.java
+++ Colla_project/src/dao/SetAlarmDao.java
@@ -5,7 +5,7 @@ import java.util.Map;
 import model.SetAlarm;
 
 public interface SetAlarmDao {
-	public int insertSetAlarm(SetAlarm setAlarm);
+	public int insertSetAlarm(int mNum);
 	public int updateSetAlarm(String type, int result,int mNum);
 	public int deleteSetAlarm(int num);
 	public SetAlarm selectSetAlarm(int mNum);
