e1cdca2a8d35d7d0b0f692ba8bd835fc78734de6
Benefit All Humankind <48048696+benefitallhumankind@users.noreply.github.com>
2019-10-21 14:45:54 +0900

    메일 비밀번호 숨김

--- Colla_project/src/mail/MailAuth.java
+++ Colla_project/src/mail/MailAuth.java
@@ -7,7 +7,7 @@ public class MailAuth extends Authenticator{
     PasswordAuthentication pa;
     public MailAuth() {
         String mail_id = "collacolla19@gmail.com";
-        String mail_pw = "wlftndjqtwh19"; 
+        String mail_pw = "메일 비밀번호"; 
         pa = new PasswordAuthentication(mail_id, mail_pw);
     }
     public PasswordAuthentication getPasswordAuthentication() {
