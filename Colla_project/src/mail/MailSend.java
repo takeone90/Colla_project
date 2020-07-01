c2b8e4ec7d561d9df05293ce6b9abdab468721a1
hyeseon-park <lightning350@naver.com>
2020-07-01 12:36:26 +0900

    method 이름 수정

--- Colla_project/src/mail/MailSend.java
+++ Colla_project/src/mail/MailSend.java
@@ -13,16 +13,18 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
  
 public class MailSend {
-    public void MailSend(String emailAddress, String code, String type) {
+    public void sendMail(String emailAddress, String code, String type) {
         Properties prop = System.getProperties();
         prop.put("mail.smtp.starttls.enable", "true");
         prop.put("mail.smtp.host", "smtp.gmail.com");
         prop.put("mail.smtp.auth", "true");
         prop.put("mail.smtp.port", "587");
+        
         Authenticator auth = new MailAuth();
         Session session = Session.getDefaultInstance(prop, auth);
         MimeMessage msg = new MimeMessage(session);
         String mailTitle = null;
+        
         if(type.equals("verifyCode")) {
         	mailTitle = "[COLLA] 회원가입 인증코드 메일입니다.";
         } else if(type.equals("resetCode")) {
@@ -30,6 +32,7 @@ public class MailSend {
         } else {
         	mailTitle = "[COLLA] 워크스페이스 초대 메일입니다.";
         }
+        
         try {
             msg.setSentDate(new Date());
             msg.setFrom(new InternetAddress("collacolla19@gmail.com", "colla"));
